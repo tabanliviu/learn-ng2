1c96d2cf66e7f8c7d346ff4cb082b007b11e7e65
Liviu Taban <tabanliviu@yahoo.com>
2020-12-16 22:20:53 +0200

    copy angular 11 version

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,38 +1,40 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Location} from '@angular/common';
-import {ActivatedRoute} from '@angular/router';
-import {Hero} from '../hero';
-import {HeroService} from '../hero.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor(private route: ActivatedRoute,
-              private heroService: HeroService,
-              private location: Location) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getHero();
   }
 
   getHero(): void {
-    const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+    const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
   save(): void {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
-  }
-
-  goBack(): void {
-    this.location.back();
   }
 }
